0a61f165196d390e278176934bd515d641eb73dc
Dejan Svetec <dejan.svetec@icloud.com>
2022-05-24 21:21:21 +0200

    Set title also from issues mentioned in body and add issue links to the body

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,67 @@
-import { getIssueIdFromBranchName } from ".";
+import { getLinearIssueIds } from ".";
 
-describe("getIssueIdFromBranchName", () => {
+let pullRequest;
 
-    test('it should parse the branch name correctly', () => {
-        const branchName = "feature/eng-952-wow-make-pr-titles-autoupdate-with";
-        const issueId = getIssueIdFromBranchName(branchName);
-        expect(issueId).toBe("ENG-952");
-    });
+beforeEach(() => {
+  pullRequest = {
+    number: 123,
+    title: "",
+    body: null,
+    head: {
+      ref: null,
+      repo: {
+        name: "bar",
+        owner: {
+          login: "foo",
+        },
+      },
+    },
+  };
+});
 
-    test('it should ignore other numbers', () => {
-        const branchName = "feature/eng-952-234-wow234-make-pr-3746-2726";
-        const issueId = getIssueIdFromBranchName(branchName);
-        expect(issueId).toBe("ENG-952");
-    });
+describe("getLinearIssueIds", () => {
+  test("it should parse the branch name correctly", () => {
+    pullRequest.head.ref = "eng-952-wow-make-pr-titles-autoupdate-with";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual(["ENG-952"]);
+  });
 
-    test('it should work with other teams', () => {
-        const branchName = "feature/pro-397-something";
-        const issueId = getIssueIdFromBranchName(branchName);
-        expect(issueId).toBe("PRO-397");
-    });
+  test("it should ignore other numbers", () => {
+    pullRequest.head.ref = "eng-952-234-wow234-make-pr-3746-2726";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual(["ENG-952"]);
+  });
 
-    test('it should return nothing upon incorrect format', () => {
-        const branchName = "nonfeature/eng-397-something";
-        const issueId = getIssueIdFromBranchName(branchName);
-        expect(issueId).toBeNull();
-    });
+  test("it should work with other teams", () => {
+    pullRequest.head.ref = "pro-397-something";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual(["PRO-397"]);
+  });
 
-})
+  test("it should return an empty list upon incorrect format", () => {
+    pullRequest.head.ref = "something/eng-397-something";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual([]);
+  });
+
+  test("it should parse the body correctly", () => {
+    pullRequest.head.ref = "foo";
+    pullRequest.body = "Much wow\nHopefully fixes ENG-123, resolves ENG-454";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual(["ENG-123", "ENG-454"]);
+  });
+
+  test("it should return an empty list if no issue is mentioned in the body", () => {
+    pullRequest.head.ref = "foo";
+    pullRequest.body = "This is a story all about how...";
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual([]);
+  });
+
+  test("it should return an empty list if the body's empty", () => {
+    pullRequest.head.ref = "foo";
+    pullRequest.body = null;
+    const issueIds = getLinearIssueIds(pullRequest);
+    expect(issueIds).toEqual([]);
+  });
+});
